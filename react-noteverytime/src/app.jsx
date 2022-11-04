b7bab90a374ad52cdc027e03b0095cd2eb47b8b3
Wansik Jo <jws5327@ajou.ac.kr>
2022-11-04 14:07:23 +0900

    test: Test react router
    !You must fix node-modules/react-scripts/paths.js
    line 93 to
      appIndexJs: resolveModule(resolveApp, 'src/layout/index'),
    
    현재 브랜치 router-setting
    커밋할 변경 사항:
    	수정함:        src/app.jsx

--- react-noteverytime/src/app.jsx
+++ react-noteverytime/src/app.jsx
@@ -1,7 +1,17 @@
 import "./app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Hi</div>}></Route>
+          <Route path="/test" element={<div>test</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
