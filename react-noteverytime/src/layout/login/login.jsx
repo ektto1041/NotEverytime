c9a8bd0af8e7262be054d4d0b14562fbd83c02b5
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-11-07 12:56:06 +0900

    feat: in Login, email, password state 추가

--- react-noteverytime/src/layout/login/login.jsx
+++ react-noteverytime/src/layout/login/login.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import './login.scss';
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // email 변경 이벤트
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  // password 변경 이벤트
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
   return (
     <div className='login-container'>
       <div className='wrapper'>
@@ -9,8 +23,8 @@ export const Login = () => {
           <div className='logo'>
             NotEverytime
           </div>
-          <input className='input' type="text" placeholder='이메일' />
-          <input className='input' type="password" placeholder='비밀번호' />
+          <input className='input' type="text" placeholder='이메일' value={email} onChange={handleEmailChange} />
+          <input className='input' type="password" placeholder='비밀번호' value={password} onChange={handlePasswordChange} />
           <button className='login-button'>login</button>
         </div>
         <div className='tip-box'>
