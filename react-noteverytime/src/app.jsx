30d81997d262f7492f6ee8be122af312a0d8af56
Wansik Jo <jws5327@ajou.ac.kr>
2022-11-08 12:54:45 +0900

    chore: Make commit for statsh merge
    현재 브랜치 components
    커밋할 변경 사항:
    	수정함:        src/app.jsx
    	새 파일:       src/components/post_card/post_card.jsx
    	새 파일:       src/components/post_card/post_card.scss

--- react-noteverytime/src/app.jsx
+++ react-noteverytime/src/app.jsx
@@ -1,13 +1,22 @@
 import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/header/header";
+import { PostCard } from "./components/post_card/post_card";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Header />}></Route>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <PostCard />
+              </>
+            }
+          ></Route>
           <Route path="/test" element={<div>test</div>}></Route>
         </Routes>
       </BrowserRouter>
