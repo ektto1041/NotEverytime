73ef08acf99abf326bb428c037de1526afabdb94
Wansik Jo <jws5327@ajou.ac.kr>
2022-11-14 18:25:25 +0900

    fix: Fix head to reveal
    <Head/> component is only not reveal in sign up or login page
    
    ---
    
    현재 브랜치 develop
    브랜치가 'origin/develop'보다 2개 커밋만큼 앞에 있습니다.
      (로컬에 있는 커밋을 제출하려면 "git push"를 사용하십시오)
    
    커밋할 변경 사항:
    	수정함:        react-noteverytime/src/layout/frame/frame.jsx
    
    ---
    
    대화형 리베이스 진행 중. 갈 위치는 00819f8
    Last commands done (3 commands done):
       pick 6984dbb feat: Make all element to box-sizing border-box
       pick b0cd112 fix: Fix head to reveal
    명령이 남아있지 않음.
    현재 'develop' 브랜치를 '00819f8' 위로 리베이스하는 중입니다.
    
    커밋할 변경 사항:
    	수정함:        react-noteverytime/src/layout/frame/frame.jsx

--- react-noteverytime/src/layout/frame/frame.jsx
+++ react-noteverytime/src/layout/frame/frame.jsx
@@ -4,6 +4,7 @@ import { Header } from "../../components/header/header.jsx";
 import { Lecture } from "../lecture/lecture.jsx";
 import { Login } from "../login/login.jsx";
 import { Main } from "../main/main.jsx";
+import { Post } from "../post/post.jsx";
 import { Register } from "../register/register.jsx";
 import "./frame.scss";
 
@@ -12,12 +13,24 @@ export const Frame = () => {
 
   return (
     <>
+<<<<<<< HEAD
       {location.pathname == "/" || location.pathname.includes("/lecture") ? <Header /> : <></>}
+=======
+      {!(
+        location.pathname.includes("/login") ||
+        location.pathname.includes("/register")
+      ) && <Header />}
+>>>>>>> b0cd112 (fix: Fix head to reveal)
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+<<<<<<< HEAD
         <Route path="/lecture/:lectureId" element={<Lecture />} />
+=======
+        <Route path="/class/:classId" element={<Class />} />
+        <Route path="/post" element={<Post />} />
+>>>>>>> b0cd112 (fix: Fix head to reveal)
       </Routes>
     </>
   );
