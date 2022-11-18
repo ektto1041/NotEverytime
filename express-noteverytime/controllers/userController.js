e042fda18da61d2931f68d922a9c0db173f5274c
moonchaewon <codnjs8989@gmail.com>
2022-11-18 22:58:56 +0900

    feat: implement api to post join

---
+++ express-noteverytime/controllers/userController.js
@@ -0,0 +1,51 @@
+const User = require("../models/user/user");
+
+const isDuplicated = async (db, field) => {
+  const existField = await db.exists({ field });
+  if (existField !== null) {
+    console.log(`${field} already exists`);
+    return true;
+  }
+  return false;
+}
+
+const isEmpty = (field) => field === '' || field === undefined;
+
+const postJoin = async (req, res) => {
+  const { _id, password, username, email, is_auth, profile_image } = req.body;
+    
+  if (isEmpty(_id)) {
+    return res.status(409).send("please input id");
+  } else if (isEmpty(password)) {
+    return res.status(409).send("please input password");
+  } else if (isEmpty(username)) {
+    return res.status(409).send("please input username");
+  } else if (isEmpty(email)) {
+    return res.status(409).send("please input email");
+  }
+
+  if (await isDuplicated(User, _id)) {
+    return res.status(409).send("id already exists");
+  } else if (await isDuplicated(User, username)) {
+    return res.status(409).send("username already exists");
+  } else if (await isDuplicated(User, email)) {
+    return res.status(409).send("email already exists");
+  }
+
+  try {
+    await User.create({
+      _id,
+      password,
+      username,
+      email,
+      is_auth,
+      profile_image,
+    });
+    return res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send(error._message);
+  }
+};
+
+module.exports = postJoin;
--- express-noteverytime/routers/rootRouter.js
+++ express-noteverytime/routers/rootRouter.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const rootRouter = express.Router();
+const postJoin = require("../controllers/userController");
 
 rootRouter.get("/", (req, res, next) => {
   res.send("rootRouter");
 });
+
+rootRouter.post("/join", postJoin);
 
 module.exports = rootRouter;
