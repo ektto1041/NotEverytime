37be9dddf54139c80de32c24d631e97ed5671227
moonchaewon <codnjs8989@gmail.com>
2022-11-21 02:50:20 +0900

    feat: add error handling in board controller

--- express-noteverytime/controllers/boardController.js
+++ express-noteverytime/controllers/boardController.js
@@ -9,19 +9,28 @@ const getBoard = async (req, res) => {
   // board_id를 통해서는 강의 정보와 게시글을 불러오고
   const { board_id } = req.params;
   const board = await Board.findById({ _id: board_id });
+  if (!board) {
+    return res.status(404).send("Board not found.");
+  }
   const lecture_id = board.lecture_id;
   const lecture = await Lecture.findById({ _id: lecture_id });
   const posts = await Post.find({board_id});
   
   // user_id를 통해서는 강의 세부 정보를 불러옴
   const user = await User.findOne({ account_id: "mcodnjs" });
+  if (!user) {
+    return res.status(404).send("User not found.");
+  }
   const user_lecture = await UserLecture.findOne({user_id: user._id}).populate('lecture_detail_id');
-  const user_lectures = user_lecture.lecture_detail_id;
-  let user_lecture_detail;
-  user_lectures.map((lecture) => {
-    lecture.lecture_id.equals(lecture_id) ? user_lecture_detail = lecture : ""
+  if (!user_lecture) {
+    return res.status(404).send("User's lecture not found.");
+  }
+  const lectures = user_lecture.lecture_detail_id;
+  let lecture_detail;
+  lectures.map((lecture) => {
+    lecture.lecture_id.equals(lecture_id) ? lecture_detail = lecture : ""
   });
-  result = { lecture, posts, user_lecture_detail}
+  result = { lecture, lecture_detail, posts }
   return res.send(result);
 };
 
