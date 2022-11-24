2dd3157dc6445546fdd9301d8088e27a4ecb2d25
moonchaewon <codnjs8989@gmail.com>
2022-11-24 22:54:13 +0900

    feat: add dotenv and db file

---
+++ express-noteverytime/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_URL, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log("DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
--- express-noteverytime/index.js
+++ express-noteverytime/index.js
@@ -1,32 +1,21 @@
 const express = require("express");
 const app = express();
-
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost:27017/noteverytime", { 
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-const handleOpen = () => console.log("Connected to DB");
-const handleError = (error) => console.log("DB Error", error);
+const db = require('./db');
+require('dotenv').config();
 
 const cors = require('cors');
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.CORS_ORIGIN,
   credentials: true
 }
 app.use(cors(corsOptions));
-
-db.on("error", handleError);
-db.once("open", handleOpen);
 
 app.use(express.json()); 
 app.use(express.urlencoded( {extended : false } ));
 
 const rootRouter = require('./routers/rootRouter');
 const lectureRouter = require('./routers/lectureRouter');
+
 app.use('/', rootRouter);
 app.use('/lecture', lectureRouter);
 
