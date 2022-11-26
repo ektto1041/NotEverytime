d0361b23b291237d8bd477ab033629951148d023
moonchaewon <codnjs8989@gmail.com>
2022-11-26 15:50:39 +0900

    feat: remove session middleware

--- express-noteverytime/routers/lectureRouter.js
+++ express-noteverytime/routers/lectureRouter.js
@@ -7,7 +7,7 @@ lectureRouter.get("/", (req, res, next) => {
   res.send("lectureRouter");
 });
 
-lectureRouter.get("/:lectureId/view", isSessionMiddleware, getBoard);
-lectureRouter.get("/:lectureId/view/:articleId", isSessionMiddleware, getArticle);
+lectureRouter.get("/:lectureId/view", getBoard);
+lectureRouter.get("/:lectureId/view/:articleId", getArticle);
 
 module.exports = lectureRouter;
