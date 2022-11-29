8962eeb99140f1923f1219315616435845ba6485
MinCrohn <minho119@ajou.ac.kr>
2022-11-29 22:03:17 +0900

    style: Fix component classItem

--- react-noteverytime/src/components/classItem/classItem.jsx
+++ react-noteverytime/src/components/classItem/classItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./classItem.scss";
-import "../../styles/fontStyle.scss"
+import "../../styles/fontStyle.scss";
+import "../../styles/globals.scss";
 
 export const ClassItem = ({ className, previewList }) => {
   return (
     <div className="class-item-container">
 
       <div className='class-item-title'>
-        <div className="h6">{className}</div>
+        <div className="h6 title-name ">{className}</div>
         <div className='Icon_RightArrow'></div>
       </div>
 
@@ -16,8 +17,8 @@ export const ClassItem = ({ className, previewList }) => {
       <div className="class-item-preview-list">
         {previewList.map((preview, idx) => (
           <div className="class-item-preview">
-            <div className="p4">{preview.title}</div>
-            <div className="p4">{preview.createdAt}</div>
+            <div className="p4 preview-title">{preview.title}</div>
+            <div className="p4 preview-createdAt">{preview.createdAt}</div>
           </div>
         ))}
       </div>
