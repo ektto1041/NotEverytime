c8014acfb5522de773adbf1e8c95136aad43a374
MinCrohn <minho119@ajou.ac.kr>
2022-11-29 21:51:50 +0900

    style: Update header page - Apply responsive design - Apply font style - Change logo & profile icon

--- react-noteverytime/src/components/header/header.jsx
+++ react-noteverytime/src/components/header/header.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./header.scss";
+import "../../styles/fontStyle.scss";
 
 export const Header = () => {
   return (
     <header className="header">
-      <div className="logo">NotEverytime</div>
-      <div className="search-bar">
-        <input type="text" placeholder="커뮤니티 검색"></input>
+      <div className="header-Container">
+        <div className="header-centerContainer">
+          <img src="/images/Logo.svg" height="32px"/>
+        </div>
+        
+        <div className="header-centerContainer search-bar">
+          <input className="p3" type="text" placeholder="커뮤니티 검색"></input>
+        </div>
+        <div className="header-centerContainer">
+          <img src="/images/account-circle.svg" height="44px"/>
+        </div>
       </div>
-      <div className="profile">profile</div>
+      
     </header>
   );
 };
