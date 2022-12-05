82894fccf3e87cf11f690ffb89f395fe3f79d1f1
Wansik Jo <jws5327@ajou.ac.kr>
2022-12-06 01:51:29 +0900

    feat : Make head logo goto main

--- react-noteverytime/src/components/header/header.jsx
+++ react-noteverytime/src/components/header/header.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "./header.scss";
 import "../../styles/fontStyle.scss";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="header-Container">
         <div className="header-centerContainer">
-          <img src="/images/Logo.svg" height="32px"/>
+          <a href="/">
+            <img src="/images/Logo.svg" height="32px" />
+          </a>
         </div>
-        
+
         <div className="header-centerContainer search-bar">
           <input className="p3" type="text" placeholder="커뮤니티 검색"></input>
         </div>
         <div className="header-centerContainer">
-          <img src="/images/account-circle.svg" height="44px"/>
+          <img src="/images/account-circle.svg" height="44px" />
         </div>
       </div>
-      
     </header>
   );
 };
