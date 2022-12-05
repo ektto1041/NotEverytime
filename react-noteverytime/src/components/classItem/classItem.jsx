fb2e6cf2e8c4ecb8a85121c6ad85ab94d22e4a71
Wansik Jo <jws5327@ajou.ac.kr>
2022-12-06 02:32:11 +0900

    feat : Make title link to lecture page

--- react-noteverytime/src/components/classItem/classItem.jsx
+++ react-noteverytime/src/components/classItem/classItem.jsx
@@ -3,6 +3,7 @@ import "./classItem.scss";
 import "../../styles/fontStyle.scss";
 import "../../styles/globals.scss";
 import { getArticles } from "../../utils/api";
+import { Link } from "react-router-dom";
 
 export const ClassItem = ({ lectureName, lectureId }) => {
   const [articles, setArticles] = useState([]);
@@ -18,8 +19,10 @@ export const ClassItem = ({ lectureName, lectureId }) => {
   return (
     <div className="class-item-container">
       <div className="class-item-title">
-        <div className="h6 title-name ">{lectureName}</div>
-        <div className="Icon_RightArrow"></div>
+        <Link className="link-lecture" to={`/lecture/${lectureId}`}>
+          <div className="h6 title-name ">{lectureName}</div>
+          <div className="Icon_RightArrow"></div>
+        </Link>
       </div>
       <div className="semester-box"></div>
       <div className="class-item-preview-list">
