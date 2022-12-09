121c09e6d229de9d0f5c15e7d25c992bc043cd8d
MinCrohn <minho2119@gmail.com>
2022-12-09 22:23:09 +0900

    style: Update articleListItem

--- react-noteverytime/src/components/articleListItem/articleListItem.jsx
+++ react-noteverytime/src/components/articleListItem/articleListItem.jsx
@@ -26,12 +26,15 @@ export const ArticleListItem = ({ article }) => {
               {content.replace(/<[^>]*>?/g, "")}
             </div>
             <div className="article-data">
-              <div className="p4 article-created-at">
-                {modifiedAt && moment.isMoment(modifiedAt)
-                  ? modifiedAt.fromNow()
-                  : ""}
+              <div className="article-data-info">
+                <div className="p4 article-created-at">
+                  {modifiedAt && moment.isMoment(modifiedAt)
+                    ? modifiedAt.fromNow()
+                    : ""}
+                </div>
+                <div className="p4 article-writer">{username}</div>
               </div>
-              <div className="p4 article-writer">{username}</div>
+              
               <div className="article-icons">
                 <div className="p4 comments">
                   <img
