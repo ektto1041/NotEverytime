e3c5dcb4d0dab9bf325b880bb209d061166d08f6
MinCrohn <minho2119@gmail.com>
2022-12-11 16:17:30 +0900

    style: Fix header search text

--- react-noteverytime/src/components/header/header.jsx
+++ react-noteverytime/src/components/header/header.jsx
@@ -39,7 +39,7 @@ export const Header = () => {
             <input
               className="p3"
               type="text"
-              placeholder="게시판 검색"
+              placeholder="강의 검색"
               value={keyword}
               onChange={(e) => setKeyword(e.target.value)}
             ></input>
